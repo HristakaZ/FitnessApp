1ce7ef074e7984e5d48a22148fecb54c295a860f
HristakaZ <hristaka2013@abv.bg>
2021-06-01 01:48:30 +0300

    Added image upload for each exercise, added property, giving the download url for the image to display for later on, because the images are stored in the cloud firebase storage.

--- FitnessApplication/app/src/main/java/com/example/fitnessapplication/ExerciseActivity.java
+++ FitnessApplication/app/src/main/java/com/example/fitnessapplication/ExerciseActivity.java
@@ -1,13 +1,18 @@
 package com.example.fitnessapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -15,16 +20,23 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import models.Exercise;
 import models.FitnessProgram;
@@ -32,14 +44,90 @@ import models.FitnessProgram;
 public class ExerciseActivity extends AppCompatActivity {
     private Spinner fitnessProgramSpinner;
     private ListView exercisesListView;
+    private ImageView exercisePicture;
+    private Uri pictureUri;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    private StorageReference pictureRef;
+    private Button submitButton;
+    private String imagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+        exercisePicture = findViewById(R.id.exercisePicture);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        submitButton = findViewById(R.id.exerciseSubmitBtn);
         getIntent();
         fitnessProgramSpinner = getFitnessPrograms();
+        exercisePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choosePicture();
+            }
+        });
     }
+
+    private void choosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            pictureUri = data.getData();
+            exercisePicture.setImageURI(pictureUri);
+            submitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    uploadPicture();
+                }
+            });
+        }
+    }
+
+
+    private void uploadPicture() {
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading image ...");
+        progressDialog.show();
+
+        final String randomKey = UUID.randomUUID().toString();
+
+        pictureRef = storageReference.child("images/" + randomKey);
+
+        imagePath = pictureRef.toString();
+
+        pictureRef.putFile(pictureUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                progressDialog.dismiss();
+                Snackbar.make(findViewById(android.R.id.content), "Image uploaded",
+                        Snackbar.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull @NotNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(getApplicationContext(), "Failed to upload", Toast.LENGTH_LONG).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull @NotNull UploadTask.TaskSnapshot snapshot) {
+                double progressPercentage = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                progressDialog.setMessage("Progress: " + (int) progressPercentage + "%");
+            }
+        });
+        createExercise(submitButton);
+    }
+
 
     public Spinner getFitnessPrograms() {
         fitnessProgramSpinner = findViewById(R.id.fitnessProgramOptionSpinner);
@@ -69,10 +157,11 @@ public class ExerciseActivity extends AppCompatActivity {
     public void createExercise(View view) {
         EditText exerciseText = findViewById(R.id.exerciseText);
         String exerciseName = exerciseText.getText().toString();
+        String cloudImagePath = imagePath;
         fitnessProgramSpinner = findViewById(R.id.fitnessProgramOptionSpinner);
         String chosenFitnessProgram = fitnessProgramSpinner.getSelectedItem().toString();
         FitnessProgram fitnessProgram = new FitnessProgram(chosenFitnessProgram);
-        Exercise exercise = new Exercise(exerciseName, fitnessProgram.getName());
+        Exercise exercise = new Exercise(exerciseName, cloudImagePath, fitnessProgram.getName());
 
         DatabaseReference exercises = FirebaseDatabase.getInstance().getReference("Exercises");
         exercises.push().setValue(exercise).addOnSuccessListener(new OnSuccessListener<Void>() {
--- FitnessApplication/app/src/main/java/models/Exercise.java
+++ FitnessApplication/app/src/main/java/models/Exercise.java
@@ -3,19 +3,22 @@ package models;
 public class Exercise {
 
     private String name;
+    private String cloudImagePath;
     private String fitnessProgram;
 
     public Exercise() {
 
     }
 
-    public Exercise(String name, String fitnessProgram) {
+    public Exercise(String name, String cloudImagePath, String fitnessProgram) {
         this.name = name;
+        this.cloudImagePath = cloudImagePath;
         this.fitnessProgram = fitnessProgram;
     }
 
     public String getName() {
         return this.name;
     }
+    public String getCloudImagePath() { return this.cloudImagePath; }
     public String getFitnessProgram() { return this.fitnessProgram; }
 }
